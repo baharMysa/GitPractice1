a5922e30f53272f96dcaa53290807873d14314f6
Bahar <bahareh_jaberi@yahoo.com>
2021-04-24 16:55:59 -0400

    Adding more work after publishing to github.

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Adding GitHub now");
-        System.out.println("yay stuff are on gitHub");
+        System.out.println("yay stuff are on gitHub.");
+        // adding final wrap up code
 
     }
 }
